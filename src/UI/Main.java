831387bdb5a72d29f98fc100831f863383a1dc9a
Ivan Spajic <ivan.spajic@gmail.com>
2020-04-21 17:49:21 +0200

    Created the first draft of the UI.

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -8,14 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private final String WINDOW_TITLE = "Medical Record Encryptor";
+    private final boolean WINDOW_RESIZABLE = false;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle(WINDOW_TITLE);
+        primaryStage.setResizable(WINDOW_RESIZABLE);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);

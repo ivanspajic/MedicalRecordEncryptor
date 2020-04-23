dac623c8393a6d77a1db9abadb2896b1bec2f0a8
Ivan Spajic <ivan.spajic@gmail.com>
2020-04-23 09:27:56 +0200

    Added event listeners for all the buttons and the check box.

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,4 +1,35 @@
 package UI;
 
+import javafx.beans.value.ChangeListener;
+import javafx.scene.Parent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    private boolean deleteFile = false;
+
+    public Controller() {}
+
+    public void sourceLocationButtonClicked(MouseEvent mouseEvent) {
+        System.out.println("source button clicked");
+    }
+
+    public void targetLocationButtonClicked(MouseEvent mouseEvent) {
+        System.out.println("target button clicked");
+    }
+
+    public void deleteSourceFileCheckBoxClicked(MouseEvent mouseEvent) {
+        CheckBox checkBox = (CheckBox) mouseEvent.getSource();
+        deleteFile = checkBox.isSelected();
+
+        System.out.println("checkbox " + deleteFile);
+    }
+
+    public void encryptButtonClicked(MouseEvent mouseEvent){
+        System.out.println("encrypt button clicked");
+    }
+
+    public void decryptButtonClicked(MouseEvent mouseEvent){
+        System.out.println("decrypt button clicked");
+    }
 }
--- src/UI/Main.java
+++ src/UI/Main.java
@@ -6,19 +6,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
 
     private final String WINDOW_TITLE = "Medical Record Encryptor";
     private final boolean WINDOW_RESIZABLE = false;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        URL xmlUrl = getClass().getResource("sample.fxml");
+
+        Parent root = FXMLLoader.load(xmlUrl);
+        FXMLLoader loader = new FXMLLoader(xmlUrl);
+
+        Controller controller = new Controller();
+
+        loader.setController(controller);
+
         primaryStage.setTitle(WINDOW_TITLE);
         primaryStage.setResizable(WINDOW_RESIZABLE);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-        //I added a comment
     }
 
     public static void main(String[] args) {

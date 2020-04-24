62be1063dd64c163ba3721833433f8fe3f425f9e
Ivan Spajic <ivan.spajic@gmail.com>
2020-04-24 17:21:03 +0200

    Added more functionality behind the UI.

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,35 +1,88 @@
 package UI;
 
 import javafx.beans.value.ChangeListener;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.CheckBox;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.List;
 
 public class Controller {
-    private boolean deleteFile = false;
+    private final String SOURCE_FILE_CHOOSER_TITLE = "Select Source File";
+    private final String TARGET_DIRECTORY_CHOOSER_TITLE = "Select Target Directory";
+
+    @FXML
+    private Button encryptButton;
+    @FXML
+    private Button decryptButton;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private Label sourceLabel;
+    @FXML
+    private TextField sourceField;
+    @FXML
+    private Label targetLabel;
+    @FXML
+    private TextField targetField;
+    @FXML
+    private Label iterationLabel;
+    @FXML
+    private ChoiceBox<String> iterationChoice;
+    @FXML
+    private CheckBox deleteFileCheck;
+    @FXML
+    private Button sourceButton;
+    @FXML
+    private Button targetButton;
 
     public Controller() {}
 
-    public void sourceLocationButtonClicked(MouseEvent mouseEvent) {
-        System.out.println("source button clicked");
+    @FXML
+    private void sourceLocationButtonClicked(MouseEvent mouseEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(SOURCE_FILE_CHOOSER_TITLE);
+
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null){
+            sourceField.setText(file.getAbsolutePath());
+        }
     }
 
-    public void targetLocationButtonClicked(MouseEvent mouseEvent) {
-        System.out.println("target button clicked");
+    @FXML
+    private void targetLocationButtonClicked(MouseEvent mouseEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle(TARGET_DIRECTORY_CHOOSER_TITLE);
+
+        File file = directoryChooser.showDialog(null);
+        if (file != null){
+            targetField.setText(file.getAbsolutePath());
+        }
     }
 
-    public void deleteSourceFileCheckBoxClicked(MouseEvent mouseEvent) {
-        CheckBox checkBox = (CheckBox) mouseEvent.getSource();
-        deleteFile = checkBox.isSelected();
+    @FXML
+    private void encryptButtonClicked(MouseEvent mouseEvent) {
+        if (checkUIFormConstraints()){
 
-        System.out.println("checkbox " + deleteFile);
+        }
     }
 
-    public void encryptButtonClicked(MouseEvent mouseEvent){
-        System.out.println("encrypt button clicked");
+    @FXML
+    private void decryptButtonClicked(MouseEvent mouseEvent) {
+        if (checkUIFormConstraints()){
+
+        }
     }
 
-    public void decryptButtonClicked(MouseEvent mouseEvent){
-        System.out.println("decrypt button clicked");
+    private boolean checkUIFormConstraints(){
+        return (sourceField.getText() != null && !sourceField.getText().isEmpty()) &&
+                (targetField.getText() != null && !targetField.getText().isEmpty()) &&
+                (passwordField.getText() != null && !passwordField.getText().isEmpty());
     }
 }
